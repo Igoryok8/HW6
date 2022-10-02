0cdff3cdbe484b792068292f9c0f2624cb7097d3
Igoryok8 <energymag@mail.ru>
2022-10-03 00:19:10 +0300

    Задание 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,14 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         }
+        System.out.println("Задание 3");
+        int year = 400;
+        if (year % 4==0 && year%100 != 0 || year%400 == 0) {
+            System.out.println(year+ " год высокосный");
+
+        }else {
+            System.out.println(year+ " год не высокосный");
+
+        }
     }
 }
