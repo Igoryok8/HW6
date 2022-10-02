0d77d32f29a25a77416fe3b2238414288d67fd56
Igoryok8 <energymag@mail.ru>
2022-10-02 23:39:40 +0300

    Задание 2 выполнено

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,26 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Домашняя работа");
         System.out.println("Задание 1");
-
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }else {
+        if (clientDeviceYear<2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        }
     }
 }
